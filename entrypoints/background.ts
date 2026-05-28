18299804f35c2d672be62df4591a76c154d75bf4
Waog <oliver.stadie@gmail.com>
2026-05-28 17:47:41 +0200

    feat: add context menu and side panel for Tender Analyzer

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,13 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.contextMenus.create({
+    id: "tender-analyzer",
+    title: "Tender Analyzer",
+    contexts: ["page"],
+  });
+
+  browser.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "tender-analyzer" && tab?.id) {
+      (browser as any).sidePanel.open({ tabId: tab.id });
+    }
+  });
 });
---
+++ entrypoints/sidepanel/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Tender Analyzer</h1>
+    </div>
+  );
+}
+
+export default App;
